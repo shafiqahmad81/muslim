12931e9a9d797ac7962666677278ab9ab4649511
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-07-10 18:44:27 +0600

    complete this project

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-import './style.css';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.mySwiper', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+   
+  });
+});
