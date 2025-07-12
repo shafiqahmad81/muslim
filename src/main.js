e61a55c6b883ef97ff3749c9ae3030bcba5ca528
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-07-12 15:47:31 +0600

    update slider cdn link

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
 
 document.addEventListener('DOMContentLoaded', function () {
   new Swiper('.mySwiper', {
